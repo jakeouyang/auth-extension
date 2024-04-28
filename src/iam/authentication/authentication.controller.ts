4efcbe1dd846f2ffd98d46f38f17c51bc710d9e4
Jake OuYang <jakeouyang@hotmail.com>
2024-04-28 09:45:26 +0800

    how to use jwt

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto';
+
+@Controller('authentication')
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignUpDto) {
+    return this.authService.signIn(signInDto);
+  }
+}
